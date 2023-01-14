a6368795a41aeaf008b97e773f0e4b6cd7ee60aa
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-14 16:05:01 +0600

    React-Widget

---
+++ Graph.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const Graph = ({ data }) => {
+    return (
+        <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+      </LineChart>
+    );
+}
+
+export default Graph;
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: React Dashboard Widget
+Plugin URI: https://example.com/react-dashboard-widget
+Description: A React.js dashboard widget for WordPress
+Version: 1.0
+Author: Shihab Ahmed Pranto
+Author URI: https://example.com
+*/
+
+if(!defined('ABSPATH')){
+   exit;
+}
+
+function react_dashboard_widget_init() {
+    wp_add_dashboard_widget(
+        'react_dashboard_widget',         // Widget slug.
+        'React Dashboard Widget',         // Title.
+        'react_dashboard_widget_display'  // Display function.
+    );
+}
+add_action( 'wp_dashboard_setup', 'react_dashboard_widget_init' );
+
+function react_dashboard_widget_display() {
+    echo '<div id="react-dashboard-widget"></div>';
+
+}
+
+function react_dashboard_widget_scripts() {
+    wp_enqueue_script( 'react', 'https://unpkg.com/react/umd/react.production.min.js', array() );
+    wp_enqueue_script( 'react-dom', 'https://unpkg.com/react-dom/umd/react-dom.production.min.js' );
+    wp_enqueue_script( 'prop-types', 'https://unpkg.com/prop-types/prop-types.min.js' );
+    wp_enqueue_script( 'recharts', 'https://unpkg.com/recharts/umd/Recharts.js' );
+    wp_enqueue_script( 'react-dashboard-widget', plugin_dir_url( __FILE__ ) . '/index.js' );
+}
+add_action( 'admin_enqueue_scripts', 'react_dashboard_widget_scripts' );
+
+// echo file_get_contents('wp-content/plugins/React-Dashboard-Widget/index.js');
