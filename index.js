f261bccaffb434ec08bc45a600d246a73eebcfd8
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-14 17:13:08 +0600

    fixed bug

--- index.js
+++ index.js
@@ -2,42 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Graph from './Graph.js';
 
-const sampleData = [
-    {
-        name: 'Page A',
-        pv: 2400,
-        uv: 1000,
-    },
-    {
-        name: 'Page B',
-        pv: 1398,
-        uv: 800,
-    },
-    {
-        name: 'Page C',
-        pv: 9800,
-        uv: 2000,
-    },
-    {
-        name: 'Page D',
-        pv: 3908,
-        uv: 800,
-    },
-    {
-        name: 'Page E',
-        pv: 4800,
-        uv: 1200,
-    },
-    {
-        name: 'Page F',
-        pv: 3800,
-        uv: 800,
-    },
-    {
-        name: 'Page G',
-        pv: 4300,
-        uv: 1700,
-    },
-];
-
-ReactDOM.render(<Graph data={sampleData} />, document.getElementById('react-dashboard-widget'));
+ReactDOM.render(<Graph data={window.sampleData} />, document.getElementById('react-dashboard-widget'));
--- index.php
+++ index.php
@@ -23,7 +23,44 @@ add_action( 'wp_dashboard_setup', 'react_dashboard_widget_init' );
 
 function react_dashboard_widget_display() {
     echo '<div id="react-dashboard-widget"></div>';
-
+    $sampleData = array(
+        array(
+            'name' => 'Page A',
+            'pv' => 2400,
+            'uv' => 1000,
+        ),
+        array(
+            'name' => 'Page B',
+            'pv' => 1398,
+            'uv' => 800,
+        ),
+        array(
+            'name' => 'Page C',
+            'pv' => 9800,
+            'uv' => 2000,
+        ),
+        array(
+            'name' => 'Page D',
+            'pv' => 3908,
+            'uv' => 800,
+        ),
+        array(
+            'name' => 'Page E',
+            'pv' => 4800,
+            'uv' => 1200,
+        ),
+        array(
+            'name' => 'Page F',
+            'pv' => 3800,
+            'uv' => 800,
+        ),
+        array(
+            'name' => 'Page G',
+            'pv' => 4300,
+            'uv' => 1700,
+        ),
+    );
+    wp_localize_script( 'react-dashboard-widget', 'sampleData', json_encode( $sampleData ) );
 }
 
 function react_dashboard_widget_scripts() {
@@ -34,5 +71,3 @@ function react_dashboard_widget_scripts() {
     wp_enqueue_script( 'react-dashboard-widget', plugin_dir_url( __FILE__ ) . '/index.js' );
 }
 add_action( 'admin_enqueue_scripts', 'react_dashboard_widget_scripts' );
-
-// echo file_get_contents('wp-content/plugins/React-Dashboard-Widget/index.js');
